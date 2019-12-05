b1dfa41cf0ab6ae9b1adbc2c7d2db5f75acec27d
Shawndell <shawndellmcgriff@gmail.com>
2019-12-05 16:01:28 -0500

    Fixed the name labels for the editor/admin sign up form

--- ViewModels/AddEditorViewModel.cs
+++ ViewModels/AddEditorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,24 @@ namespace ProjectForHealing.ViewModels
 {
     public class AddEditorViewModel
     {
+        [Required]
+        [Display(Name = "Username")]
         public string UserName { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
         public string Fname { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string Lname { get; set; }
+        [Required]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
+    
         public string Pnumber { get; set; }
+        [Required]
+        [Display(Name = "Password")]
         public string EditorPassWord { get; set; }
+        
         public byte[] ProfilePicUrl { get; set; }
     }
 }
