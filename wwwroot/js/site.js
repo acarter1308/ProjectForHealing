2ef81a93dde1a57f570883f94843d16304a19cd0
Austin Carter <abc031998@gmail.com>
2020-01-22 20:07:23 -0500

    Fixed Google Maps api not being loaded

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var map;
+
+function myMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+}
